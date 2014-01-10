fdb32dd086be381377f97403b3da99326ed4c485
Peter Monks <pmonks@gmail.com>
2014-01-09 18:12:27 -0800

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,32 @@
+;
+; Copyright © 2013 Peter Monks (pmonks@gmail.com)
+;
+; This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+; Unported License. To view a copy of this license, visit
+; http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative
+; Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+;
+
+(defproject bookmark-writer "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :description "A simple and dumb Clojure library that provides the ability to set the text values of bookmarks in Word 2007 (.docx) format files."
+  :url "https://github.com/pmonks/bookmark-writer"
+  :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License."
+            :url "http://creativecommons.org/licenses/by-sa/3.0/"}
+  :javac-target "1.7"
+  :dependencies [
+                  [org.clojure/clojure                  "1.5.1"]
+                  [org.clojure/tools.cli                "0.3.1"]
+                  [org.clojure/tools.trace              "0.7.6"]
+                  [org.clojure/tools.logging            "0.2.6"]
+                  [ch.qos.logback/logback-classic       "1.0.13"]
+                  [org.docx4j/docx4j                    "3.0.0" :exclusions [org.slf4j/slf4j-log4j12]]
+                ]
+  :profiles {:dev {:dependencies [
+                                   [midje "1.6.0"]
+                                   [clj-ns-browser "1.3.1"]
+                                 ]}}
+  :resource-paths ["config"]
+  :java-source-paths ["src/java"]
+  :jvm-opts ^:replace []  ; Stop Leiningen from turning off JVM optimisations - makes it slower to start but ensures code runs as fast as possible
+  :main bookmark-writer.core)
---
+++ src/bookmark_writer/core.clj
@@ -0,0 +1,52 @@
+;
+; Copyright © 2013,2014 Peter Monks (pmonks@gmail.com)
+;
+; This work is licensed under the Creative Commons Attribution-ShareAlike 3.0
+; Unported License. To view a copy of this license, visit
+; http://creativecommons.org/licenses/by-sa/3.0/ or send a letter to Creative
+; Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+;
+
+(ns bookmark-writer.core
+  (:require [clojure.tools.logging :as log])
+  (:gen-class))
+
+(def ^:private template-filename "test/test.docx")
+(def ^:private output-filename "test/testoutput.safetodelete.docx")
+(def ^:private bookmark-map { "BM_Text"             "#THIS TEXT WAS INSERTED INTO BOOKMARK \"BM_Text\"#",
+                              "BM_NoText"           "#THIS TEXT WAS INSERTED INTO BOOKMARK \"BM_NoText\"#",
+                              "BM_InTable"          "#THIS TEXT WAS INSERTED INTO BOOKMARK \"BM_InTable\"#",
+                              "NonExistentBookmark" "!!THIS TEXT SHOULD NOT BE VISIBLE ANYWHERE!!" })
+
+(defn- make-stupid-java-map
+  [bookmark-name-value-map]
+  (reduce merge (map #(hash-map (org.docx4j.model.fields.merge.DataFieldName. %) (get bookmark-name-value-map %)) (keys bookmark-name-value-map))))
+
+(defn- replace-bookmark-text!
+  [^org.docx4j.openpackaging.packages.WordprocessingMLPackage wordml-pkg
+   ^java.util.Map bookmark-name-value-map]
+  (let [corrected-map (make-stupid-java-map bookmark-name-value-map)]
+    (org.docx4j.samples.BookmarksReplaceWithText/replaceBookmarkContents (-> wordml-pkg .getMainDocumentPart
+                                                                                        .getJaxbElement
+                                                                                        .getBody
+                                                                                        .getContent)
+                                                                         corrected-map)))
+
+(defn populate-bookmarks!
+  "Reads the file identified by input-document (which must be a valid Office XML document), replaces the text
+   of all bookmarks in that document with the values defined in the bookmark-map (keys=bookmark names, values=desired text
+   for those bookmarks), and then writes the resulting Word document to the file identified by output-document."
+  [input-document
+   output-document
+   bookmark-map]
+  (let [template-document (org.docx4j.Docx4J/load (clojure.java.io/file input-document))]
+    (replace-bookmark-text! template-document bookmark-map) 
+    (org.docx4j.Docx4J/save template-document (clojure.java.io/file output-document) org.docx4j.Docx4J/FLAG_SAVE_ZIP_FILE)))
+
+(defn -main
+  "Simple and dumb test case."
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+
+  (populate-bookmarks! template-filename output-filename bookmark-map))
