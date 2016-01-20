5e6f3fa7f9f5d96db30b49875030be0c3f1a429e
Peter Monks <pmonks@gmail.com>
2016-01-19 22:06:26 -0800

    :arrow_up: Upgraded dependencies

--- project.clj
+++ project.clj
@@ -16,13 +16,13 @@
                      :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [
-                  [org.clojure/clojure            "1.6.0"]
+                  [org.clojure/clojure            "1.8.0"]
                   [org.clojure/tools.logging      "0.3.1"]
                   [ch.qos.logback/logback-classic "1.1.3"]
-                  [org.docx4j/docx4j              "3.2.1" :exclusions [log4j org.slf4j/slf4j-log4j12]]
+                  [org.docx4j/docx4j              "3.2.2" :exclusions [log4j org.slf4j/slf4j-log4j12]]
                 ]
-  :profiles {:dev {:dependencies [[midje      "1.6.3"]]
-                   :plugins      [[lein-midje "3.1.3"]]}   ; Don't remove this or travis-ci will assplode!
+  :profiles {:dev {:dependencies [[midje      "1.8.3"]]
+                   :plugins      [[lein-midje "3.2"]]}   ; Don't remove this or travis-ci will assplode!
              :uberjar {:aot :all}}
   :uberjar-merge-with {#"META-INF/services/.*" [slurp str spit]}   ; Awaiting Leiningen 2.3.5 - see https://github.com/technomancy/leiningen/issues/1455
   :source-paths      ["src/clojure"]
